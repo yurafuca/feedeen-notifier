3f3af3c88bc1892cc351e36797d9e1ebf4509bf3
amagomori <madokawaii@gmail.com>
2015-12-31 03:44:10 +0900

    :art: cleanup code

--- feedeen.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    alert('hoge');
-});
-
-function showTitle() {
-    alert("hey");
-}
--- popup.js
+++
@@ -1,9 +0,0 @@
-// feedeen Unreads Counter
-// author: furafy
-// version: 1.0
-// date: 15/12/28
-// description: display unreads count of feedeen.com to extension's badge.
-
-window.onload = function() {
-	window.location.replace("http://stackoverflow.com");
-};
