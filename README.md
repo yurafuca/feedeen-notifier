8d9161d183610e5778d945c7a490331b99605180
amagomori <madokawaii@gmail.com>
2015-12-31 21:59:52 +0900

    :pencil2: edit readme.md

--- README.md
+++ README.md
@@ -5,7 +5,11 @@ feedeen.comの未読数をツールバー上に表示します．
 
 ## Description
 
+#未読数の表示
 ツールバーに追加されるボタンをクリックするとfeedeen.comが開かれます．feedeen-notifierは，このとき開かれたタブを監視し未読数を取得，表示します．
+
+#新着通知
+feedeen-notifierは新着通知に対応しています．新たなフィードが見つかったとき，rich desktop notifications により，新着フィードの件数を確認することができます．
 
 ## Demo
 
--- background.js
+++ background.js
@@ -10,12 +10,8 @@ chrome.alarms.create("reload", {periodInMinutes: 1} );
 chrome.browserAction.setBadgeBackgroundColor({ color: "#498AF4"});
 reflectUnreadCount('+');
 
-chrome.alarms.onAlarm.addListener(function(alarm) {
-     if ( alarm.name == "reload" ) {
-          if (feedeenTab != null) {
-              chrome.tabs.update(feedeenTab.id, {url: "https://www.feedeen.com/d#starred"}, function() {});
-          }
-     }
+chrome.browserAction.onClicked.addListener(function(tab) {
+  run();
 });
 
 function run() {
@@ -32,37 +28,10 @@ function createFeedeenTab() {
     });
 }
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-  run();
-});
-
-chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-    if (feedeenTab != null) {
-        if (tabId == feedeenTab.id) {
-            createFeedeenTab();
-        }
-    }
-});
-
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (tabId == feedeenTab.id) {
-        if (tab.status == "complete") {
-            //console.log("created tab's id: " + feedeenTab.id);
-            getUnreadCount(feedeenTab);
-        }
-    }
-});
-
 function reflectUnreadCount(unreads) {
     //console.log('unreads:' + unreads);
     chrome.browserAction.setBadgeText({text: String(unreads)});
 }
-
-chrome.notifications.onClicked.addListener(function (notificationId) {
-    if (notificationId == 'newItem') {
-        run();
-    }
-});
 
 function getUnreadCount(tab) {
     chrome.tabs.sendMessage(tab.id, {method: "getUnreads"}, function(response) {
@@ -93,3 +62,35 @@ function getUnreadCount(tab) {
         }
   });
 }
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (tabId == feedeenTab.id) {
+        if (tab.status == "complete") {
+            //console.log("created tab's id: " + feedeenTab.id);
+            getUnreadCount(feedeenTab);
+        }
+    }
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+    if (feedeenTab != null) {
+        if (tabId == feedeenTab.id) {
+            createFeedeenTab();
+        }
+    }
+});
+
+chrome.alarms.onAlarm.addListener(function(alarm) {
+     if ( alarm.name == "reload" ) {
+          if (feedeenTab != null) {
+              chrome.tabs.update(feedeenTab.id, {url: "https://www.feedeen.com/d#starred"}, function() {});
+          }
+     }
+});
+
+chrome.notifications.onClicked.addListener(function (notificationId) {
+    if (notificationId == 'newItem') {
+        chrome.notifications.clear(notificationId, function() {});
+        run();
+    }
+});
